37d6564956a2ef16f592b8a4a9a997830569b9e5
Pascal Geuter <pascal.geuter@outlook.de>
2024-01-06 16:31:02 +0100

    add templates for the useage with acpi triggers and improve the script

--- README.md
+++ README.md
@@ -4,8 +4,54 @@
 
 Minimal replica of https://github.com/JPersson77/LGTVCompanion in python to run on linux
 
+## Features
+
+- multi plattform including linux
+- can e.g. turn on/off the screnn, turn off the system
+- can check if the HDMI input is used and only than turn the screen off
+- sends only the screen off command if the screen if on and the other way around
+- easy expandable
+
 ## Usage
 
-- clone repo
-- run `pip install -r requirements.txt`
-- run `python lg_tv_linux_companion.py --help`
+### Manually
+
+- create a dir, which normal users can read and write, I will use ~/lg-companion: `mkdir ~/lg-companion`
+- `cd ~/lg-companion`
+- `git clone https://github.com/cali-95/LGTVLinuxCompanion.git`
+- `python -m venv my_venv`
+- `source my_venv/bin/activate`
+- `pip install -r requirements.txt`
+- run `python lg_tv_linux_companion.py -c turnOffScreen -k wrong_key -t <ip>` with your ip and conform the popup on your tv.
+- take note of the client key in the log file ~/lg-companion/log.txt
+- run the same commend with the right client-key, your lg tv should go black, active it again by pressing some button on the remote
+- find other options with `python lg_tv_linux_companion.py -h`
+
+### Automatic (use triggers)
+
+thanks @fulalas for the suggestion
+
+perform the steps unter manually and then the following ones
+
+- install acpid and enable it `sudo systemctl enable acpid`
+- create a file called client-key with your client key: `echo -n "to_be_replaced" > client_key`
+- `echo -n "to_be_replaced" > ip`
+- `sed -e "s#the_user#$(whoami)#g" -e "s#the_dir#$(pwd)#g" monitor_on.dist > monitor_on`
+- `sed -e "s#the_user#$(whoami)#g" -e "s#the_dir#$(pwd)#g" monitor_off.dist > monitor_off`
+- `chmod +x monitor_on && chmod +x monitor_off`
+- `sudo mv monitor_{on,off} /etc/acpi/events/`
+- `chmod +x run_in_venv.sh`
+- reboot
+- enjoy
+
+## Troubleshooting
+
+### the trigger is not triggered
+run `acpi_listen` to find the right acpi event and change it in the monitor_(on|off) files
+
+### change to the monitor_(on|off) files are not activ
+a reboot is required
+
+## Disclaimer
+
+use the software at your own risk
--- lg_tv_linux_companion.py
+++ lg_tv_linux_companion.py
@@ -12,20 +12,24 @@ to get a client_key run the script with an invaild client_key like 1337 and look
 based on https://github.com/JPersson77/LGTVCompanion
 """
 
-import logging
 import os
 import argparse
 
-import asyncio
 from websockets.sync.client import connect
 
 import ssl
 
 import json
 
-LEVEL = logging.INFO
-FORMAT = '%(asctime)s %(message)s'
+import time
 
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from logging import Formatter
+
+FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_LEVEL = logging.DEBUG
+logger = None
 
 LG_HANDSHAKE_PAIRED = """{"type":"register","id":"register_42","payload":{"forcePairing":false,"pairingType":"PROMPT","client-key":"<client_key>","manifest":{"manifestVersion":1,"appVersion":"1.1","signed":{"created":"20140509","appId":"com.lge.test","vendorId":"com.lge","localizedAppNames":{"":"LG Remote App","ko-KR":"리모컨 앱","zxx-XX":"ЛГ Rэмotэ AПП"},"localizedVendorNames":{"":"LG Electronics"},"permissions":["TEST_SECURE","CONTROL_INPUT_TEXT","CONTROL_MOUSE_AND_KEYBOARD","READ_INSTALLED_APPS","READ_LGE_SDX","READ_NOTIFICATIONS","SEARCH","WRITE_SETTINGS","WRITE_NOTIFICATION_ALERT","CONTROL_POWER","READ_CURRENT_CHANNEL","READ_RUNNING_APPS","READ_UPDATE_INFO","UPDATE_FROM_REMOTE_APP","READ_LGE_TV_INPUT_EVENTS","READ_TV_CURRENT_TIME"],"serial":"2f930e2d2cfe083771f68e4fe7bb07"},"permissions":["LAUNCH","LAUNCH_WEBAPP","APP_TO_APP","CLOSE","TEST_OPEN","TEST_PROTECTED","CONTROL_AUDIO","CONTROL_DISPLAY","CONTROL_INPUT_JOYSTICK","CONTROL_INPUT_MEDIA_RECORDING","CONTROL_INPUT_MEDIA_PLAYBACK","CONTROL_INPUT_TV","CONTROL_POWER","CONTROL_TV_SCREEN","READ_APP_STATUS","READ_CURRENT_CHANNEL","READ_INPUT_DEVICE_LIST","READ_NETWORK_STATE","READ_RUNNING_APPS","READ_TV_CHANNEL_LIST","WRITE_NOTIFICATION_TOAST","READ_POWER_STATE","READ_COUNTRY_INFO","READ_SETTINGS"],"signatures":[{"signatureVersion":1,"signature":"eyJhbGdvcml0aG0iOiJSU0EtU0hBMjU2Iiwia2V5SWQiOiJ0ZXN0LXNpZ25pbmctY2VydCIsInNpZ25hdHVyZVZlcnNpb24iOjF9.hrVRgjCwXVvE2OOSpDZ58hR+59aFNwYDyjQgKk3auukd7pcegmE2CzPCa0bJ0ZsRAcKkCTJrWo5iDzNhMBWRyaMOv5zWSrthlf7G128qvIlpMT0YNY+n/FaOHE73uLrS/g7swl3/qH/BGFG2Hu4RlL48eb3lLKqTt2xKHdCs6Cd4RMfJPYnzgvI4BNrFUKsjkcu+WD4OO2A27Pq1n50cMchmcaXadJhGrOqH5YmHdOCj5NSHzJYrsW0HPlpuAx/ECMeIZYDh6RMqaFM2DXzdKX9NmmyqzJ3o/0lkk/N97gfVRLW5hA29yeAwaCViZNCP8iC9aO0q9fQojoa7NQnAtw=="}]}}}"""
 
@@ -37,13 +41,20 @@ ws_connection = None
 TURN_ON_SCREEN = "turnOnScreen"
 TURN_OFF_SCREEN = "turnOffScreen"
 GET_POWER_STATE = "getPowerState"
+GET_FOREGROUND_APP_INFO = "getForegroundAppInfo"
+TURN_ON_SYSTEM = "turnOnSystem"
 
+# from https://github.com/JPersson77/LGTVCompanion/blob/d3aad09d07593ed734aae40a092d86b27b43e93e/Common/LgApi.h
 command_mapping = {
     TURN_ON_SCREEN: "com.webos.service.tvpower/power/turnOnScreen",
     TURN_OFF_SCREEN: "com.webos.service.tvpower/power/turnOffScreen",
-    "turnOnSystem": "system/turnOff",
-    "turnOffSystem": "system/turnOn",
-    GET_POWER_STATE: "com.webos.service.tvpower/power/getPowerState"
+    TURN_ON_SYSTEM: "system/turnOn",
+    "turnOffSystem": "system/turnOff",
+    GET_POWER_STATE: "com.webos.service.tvpower/power/getPowerState",
+    "systemInfo": "system/getSystemInfo",
+    "getAppState": "system.launcher/getAppState",
+    "getExternalInputList": "tv/getExternalInputList",
+    GET_FOREGROUND_APP_INFO : "com.webos.applicationManager/getForegroundAppInfo"
 }
 
 def send_lg_uri(lg_uri):
@@ -52,13 +63,32 @@ def send_lg_uri(lg_uri):
     cmd_object["type"] = "request"
     cmd_object["uri"] = f"ssap://{lg_uri}"
     to_send = json.dumps(cmd_object)
-    return send_string(to_send)
+    msg =  send_string(to_send)
+
+    try:
+        msg_as_json = json.loads(msg)
+    except:
+        msg_as_json = msg
+
+    return msg_as_json
+
 
 def send_string(to_send):
     ws_connection.send(to_send)
-
     msg = ws_connection.recv()
+    logger.debug(msg)    
     return msg
+
+def create_ws_connection(connection_string, ssl_context):
+    try:
+        ws_connection_local = connect(connection_string, ssl_context=ssl_context)
+    except (Exception, OSError) as e:
+        clean_up()
+        logger.error(e, exc_info=True)
+        logger.info("you may check network connection, the ip and try the other ssl setting")
+        return None
+
+    return ws_connection_local
 
 def init(args):
 
@@ -71,11 +101,15 @@ def init(args):
 
     log_file_path = os.path.join(working_dir, "log.txt")
 
-    logging.basicConfig(filename=log_file_path, level=LEVEL, format=FORMAT)
+    global logger
+    logger = logging.getLogger(__name__)
+    handler = TimedRotatingFileHandler(filename=log_file_path, when='D', interval=1, backupCount=1, encoding='utf-8', delay=False)
+    formatter = Formatter(fmt=FORMAT)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(LOG_LEVEL)
 
-    logging.debug(f"using {working_dir} as the working dir")
-
-    global ws_connection
+    logger.debug(f"intit called with {args=}")
 
     ssl_context = None
     connection_string = None
@@ -90,25 +124,30 @@ def init(args):
         ssl_context = None
         connection_string = f"ws://{target_ip}:{WEBSOCKET_NON_SSL_PORT}"
   
-    try:
-        ws_connection = connect(connection_string, ssl_context=ssl_context)
-    except Exception:
-        clean_up()
-        logging.error("websocket connection failed, check ip and try the other ssl setting")
-        raise
 
+    for i in range(1, args.retries+2):
+        global ws_connection
+        ws_connection = create_ws_connection(connection_string, ssl_context)
+        if ws_connection != None:
+            logger.debug(f"{i} tries were needed to connect")
+            break
+        time.sleep(1.1)
+
+    if ws_connection == None:
+        clean_up_and_exit_with_error(f"connecting failed with {args.retries=}, exiting")
+        
     handshake = LG_HANDSHAKE_PAIRED.replace("<client_key>", client_key)
     
     msg = send_string(handshake)
     
     res = json.loads(msg)
     if res["type"] != "registered":
-        logging.info("after user approval, the next line should contain the valid client key")
+        logger.info("after user approval, the next line should contain the valid client key")
         second_response = ws_connection.recv()
-        logging.info(second_response)
-        clean_up_and_raise_exception("invalid client_key")
+        logger.info(second_response)
+        clean_up_and_exit_with_error("invalid client_key")
 
-    logging.info("init finished")
+    logger.info("init finished")
 
 
 def parse_arguments():
@@ -118,17 +157,18 @@ def parse_arguments():
 
     parser.add_argument("-t", "--target_ip", required=True)
     parser.add_argument("-c", "--command", required=True, choices=list(command_mapping.keys()))
-    parser.add_argument("-w", "--working_dir", default=os.path.join(os.path.expanduser('~'), "lg_tv_linux_companion"), help="default is '~/lg_tv_linux_companion'")
+    parser.add_argument("-w", "--working_dir", default=os.path.join(os.path.expanduser('~'), "lg-companion"), help="default is 'lg-companion'")
     parser.add_argument("-k", "--client_key", required=True)
     parser.add_argument("-s", "--ssl_use", default=True, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-p", "--app_usage_prevent_execution", default=True, action=argparse.BooleanOptionalAction)
+    parser.add_argument("-r", "--retries", default=1, type=int)
     
     args = parser.parse_args()
     return args
 
 def is_display_on(lg_uri):
-    msg = send_lg_uri(lg_uri)
-    msg_object = json.loads(msg)
-    logging.debug(msg_object)
+    msg_object = send_lg_uri(lg_uri)
+    logger.debug(msg_object)
     return msg_object["type"] == "response" and msg_object["payload"]["state"] == "Active"
 
 def run_display_off(lg_uri_action, lg_uri_state):
@@ -146,47 +186,68 @@ def change_display_state(lg_uri_action, lg_uri_state, wanted_first_state, wanted
     if isOn == wanted_first_state:
         send_lg_uri(lg_uri_action)
     else:
-        clean_up_and_raise_exception("wrong first state")
+        clean_up_and_exit_with_error("wrong first state")
 
     isOn = is_display_on(lg_uri_state)
     if isOn == wanted_second_state:
-        logging.debug(f"display state change '{lg_uri_action}' was successful")
+        logger.info(f"display state change '{lg_uri_action}' was successful")
     else:
-        clean_up_and_raise_exception("display state change was not successful")
+        clean_up_and_exit_with_error("display state change was not successful")
 
-def run_command(args):
-    command = args.command
-    logging.info(f"run '{command}' command")
+def run_command(command):
+    logger.info(f"run '{command}' command")
 
 
     lg_uri = command_mapping[command]
     if command == TURN_OFF_SCREEN:
-        run_display_off(lg_uri, command_mapping[GET_POWER_STATE])
+        return run_display_off(lg_uri, command_mapping[GET_POWER_STATE])
 
     elif command == TURN_ON_SCREEN:
-        run_display_on(lg_uri, command_mapping[GET_POWER_STATE])
+        return run_display_on(lg_uri, command_mapping[GET_POWER_STATE])
 
     elif lg_uri:
-        send_lg_uri(lg_uri)
+        return send_lg_uri(lg_uri)
     else:
-        clean_up_and_raise_exception("unknown command")
+        clean_up_and_exit_with_error("unknown command")
 
 
 def clean_up():
     if ws_connection:
         ws_connection.close()
-    logging.info("cleanup finished")
+    logger.info("cleanup finished")
 
-def clean_up_and_raise_exception(exception_message):
+def clean_up_and_exit_with_error(exception_message):
     clean_up()
-    raise Exception(exception_message)
+    logger.critical(exception_message)
+    exit(1)
+
+def main_with_args(args):
+
+    init(args)
+    shouldExecute = None
+    skipAppUsageCheck = (args.command == TURN_ON_SYSTEM)
+ 
+    if skipAppUsageCheck:
+        shouldExecute = True
+    elif args.app_usage_prevent_execution:
+        msg_as_json = run_command(GET_FOREGROUND_APP_INFO)
+        if "hdmi" in msg_as_json["payload"]["appId"]:
+            shouldExecute = True
+        else:
+            shouldExecute = False
+            logger.info(f"the {args.command} command is *not* executed, because a webOS app was in the foreground")
+    else:
+        shouldExecute = True
+    
+    r = None
+    if shouldExecute:
+        r = run_command(args.command)
+    clean_up()
+    return r
 
 def main():
     args = parse_arguments()
-    init(args)
-    run_command(args)
-    clean_up()
-
+    main_with_args(args)
 
 if __name__ == "__main__":
     main()
